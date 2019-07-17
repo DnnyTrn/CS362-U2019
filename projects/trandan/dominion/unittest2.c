c70fc9f13f36cd0e3d5612dce7a7215c8cc70783
Danny Tran <dannyytran@gmail.com>
2019-07-17 10:21:57 -0700

    finished test 1

--- projects/trandan/dominion/unittest2.c
+++ projects/trandan/dominion/unittest2.c
@@ -18,19 +18,27 @@ int main()
     printf("\n----------------- Testing Card: %s ----------------\n", TESTCARD);
     // initialization variables
     struct gameState test_game;
-    int numPlayers = 2, seed = 1, choice1 = 1, currentPlayer = 0;
+    int numPlayers = 2, seed = 1, choice1 = 1, choice2 = 2, currentPlayer = 0;
     int minionHandPos = 0;
     int cards[10] = {ambassador, baron, embargo, village, minion, mine, cutpurse,
                      sea_hag, tribute, smithy};
     initializeGame(numPlayers, cards, seed, &test_game);
     int handCount = test_game.handCount[currentPlayer];
     int preStateCoins = test_game.coins;
-
+    int preStateNumAction = test_game.numActions;
+    int preStateDiscardCount = test_game.discardCount[currentPlayer];
+    // printf(" discardcount %d \n ", preStateDiscardCount );
     //put minion card in player handPos 0
     test_game.hand[currentPlayer][0] = minion;
+
+
     playMinion(&test_game, choice1, currentPlayer, minionHandPos);
 
     //Test 1: Player's actions +1
+    int expectedNumAction = preStateNumAction + 1;
+    int numActions = test_game.numActions;
+    printf("Test 1: Player Actions is %d, Expected Actions is %d \n", numActions, expectedNumAction);
+    ASSERT(numActions == expectedNumAction, "Test 1 failed, number of actions is not correct value");
 
     //Test 2: check hand for minion card, minion is discarded after play
         // printf(" card before %d %d\n ",i, hand[i] );
@@ -41,6 +49,8 @@ int main()
         currentCard = test_game.hand[currentPlayer][i];
         ASSERT(currentCard != minion, "Test 2 failed, minion card still in hand");
     }
+    printf(" playedCardCount++; %d \n ", test_game.playedCardCount);
+    // ASSERT(test_game.playedCardCount == 0, "Test 2 failed, state played card count is incorrect value");
 
     //Test 3: check if choice1 gives player +2 coins
     int coinCount = test_game.coins;
@@ -51,6 +61,9 @@ int main()
     ASSERT(preStateCoins + 2 == coinCount, "Test 3: FAILED ** preStateCoins + 2 does not match coinCount");
 
     // Test 4:
+
+    printf(" discardcount %d \n ", test_game.discardCount[currentPlayer]);
+
     printf("** Unit test 2 concluded. **\n");
     return 0;
 }
