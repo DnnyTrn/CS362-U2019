3792b586031d6b9410c965e8b8d79679c627622d
Danny Tran <dannyytran@gmail.com>
2019-07-20 13:36:23 -0700

    finished writing tests

--- projects/trandan/dominion/cardtest3.c
+++ projects/trandan/dominion/cardtest3.c
@@ -39,18 +39,28 @@ int main()
     initializeGame(numPlayers, cards, 1, &g);
 
     printf("Test 1: Test to see if previous player’s hand is discarded after endTurn.\n");
+    printf("Test 2: Testing for correct player turn\n");
+    printf("Test 3: Testing for correct player variables intialization\n");
     for (i = 0; i < testRun; i++)
     {
         for (j = 0; j < numPlayers; j++)
         {
+            ASSERT(j == g.whoseTurn, "Test 2 failed: error in determining next player");
+            ASSERT(g.handCount[j] == 5, " handCount");  //current player should have 5 cards
+
             endTurn(&g);
+            // testing the player's initialization variables set by endTurn
+            ASSERT(g.outpostPlayed == 0, "current player outpostPlayed is not set to 0");
+            ASSERT(g.phase == 0, " current player phase is not set to 0");
+            ASSERT(g.numActions == 1, "current player numActions is not set to 1");
+            ASSERT(g.numBuys == 1, "current player numBuys is not set to 1");
+            ASSERT(g.playedCardCount == 0, "current player playedCardCount is not set to 0");
+
+            // checking for previous player hand is discarded/empty
             for (k = 0; k < handCount; k++)
                 ASSERT(g.hand[j][k] == -1, "Test 1 failed: Previous player's hand was not discarded");
         }
     }
-
-
-    
     printf("** Card test 3 concluded. **\n");
     return 0;
 }
