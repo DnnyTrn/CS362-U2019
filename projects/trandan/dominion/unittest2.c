396982a0a5f39623cc9674bb992e356166b734e3
Danny Tran <dannyytran@gmail.com>
2019-07-21 23:03:54 -0700

    compelted unit test

--- projects/trandan/dominion/unittest2.c
+++ projects/trandan/dominion/unittest2.c
@@ -18,7 +18,7 @@ int main()
     printf("\n----------------- Testing Card: %s ----------------\n", TESTCARD);
     // initialization variables
     struct gameState test_game;
-    int numPlayers = 2, seed = 1, choice1 = 1, choice2 = 2, currentPlayer = 0;
+    int numPlayers = 2, seed = 1, choice1 = 1,  currentPlayer = 0;
     int minionHandPos = 0;
     int cards[10] = {ambassador, baron, embargo, village, minion, mine, cutpurse,
                      sea_hag, tribute, smithy};
@@ -26,11 +26,9 @@ int main()
     int handCount = test_game.handCount[currentPlayer];
     int preStateCoins = test_game.coins;
     int preStateNumAction = test_game.numActions;
-    int preStateDiscardCount = test_game.discardCount[currentPlayer];
-    // printf(" discardcount %d \n ", preStateDiscardCount );
+
     //put minion card in player handPos 0
     test_game.hand[currentPlayer][0] = minion;
-
 
     playMinion(&test_game, choice1, currentPlayer, minionHandPos);
 
@@ -41,7 +39,6 @@ int main()
     ASSERT(numActions == expectedNumAction, "Test 1 failed, number of actions is not correct value");
 
     //Test 2: check hand for minion card, minion is discarded after play
-        // printf(" card before %d %d\n ",i, hand[i] );
     printf("Test 2: checking hand for minion card, minion should be discarded from hand\n");
     int currentCard;
     for (int i = 0; i < handCount; i++)
@@ -49,8 +46,6 @@ int main()
         currentCard = test_game.hand[currentPlayer][i];
         ASSERT(currentCard != minion, "Test 2 failed, minion card still in hand");
     }
-    printf(" playedCardCount++; %d \n ", test_game.playedCardCount);
-    // ASSERT(test_game.playedCardCount == 0, "Test 2 failed, state played card count is incorrect value");
 
     //Test 3: check if choice1 gives player +2 coins
     int coinCount = test_game.coins;
@@ -59,10 +54,6 @@ int main()
     ASSERT(choice1 == 1, "Test 3: choice1 variable is not 1 which designates this path");
     ASSERT(coinCount == expectedCoinCount, "Test 3: FAILED ** COIN COUNT does not match expectedCoinCount");
     ASSERT(preStateCoins + 2 == coinCount, "Test 3: FAILED ** preStateCoins + 2 does not match coinCount");
-
-    // Test 4:
-
-    printf(" discardcount %d \n ", test_game.discardCount[currentPlayer]);
 
     printf("** Unit test 2 concluded. **\n");
     return 0;
